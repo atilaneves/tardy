35ed29a2e6872caebeb51fab1b1309a48e30a3b7
Atila Neves <atila.neves@gmail.com>
2020-04-29 19:38:26 +0200

    Use reflection to declare vtable members

--- source/foo.d
+++ source/foo.d
@@ -19,8 +19,20 @@ struct Polymorphic(Interface) {
 }
 
 
-struct VirtualTable(T) {
-    int function(const void* self, int i) transform;
+struct VirtualTable(Interface) if(is(Interface == interface)) {
+    // FIXME:
+    // * argument defaults e.g. int i = 42
+    // * `this` modifiers (const, scope, ...)
+    // * @safe pure
+    // * overloads
+    import std.traits: ReturnType, Parameters;
+
+    private enum member(string name) = __traits(getMember, Interface, name);
+
+    static foreach(name; __traits(allMembers, Interface)) {
+        // FIXME: decide when to use void* vs const void*
+        mixin(`ReturnType!(Interface.`, name, `) function(const void*, Parameters!(Interface.`, name, `)) `, name, `;`);
+    }
 }
 
 

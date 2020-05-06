e62c8b2e2826cdb8dddc383d3e0a02e1736f921a
Atila Neves <atila.neves@gmail.com>
2020-05-06 15:35:54 +0200

    First tests for methodRecipe

--- source/tardy/refraction.d
+++ source/tardy/refraction.d
@@ -5,3 +5,19 @@ auto refractMixin(alias F)(in string newName) {
     import bolts.experimental.refraction: refract;
     return refract!(F, __traits(identifier, F)).setName(newName).mixture;
 }
+
+
+/**
+   Given a function F, constructs a mixin string for a function
+   pointer declaration that mimics that function's signature, adding a
+   first explicit void* parameter analogous to `this`.
+ */
+string methodRecipe(alias F)()
+    in(__ctfe)
+    do
+{
+    import std.conv: text;
+    import std.traits: fullyQualifiedName;
+    enum fqn = fullyQualifiedName!F;
+    return text(`std.traits.ReturnType!(`, fqn, `) function(void*)`);
+}
--- tests/ut/refraction.d
+++ tests/ut/refraction.d
@@ -2,7 +2,7 @@ module ut.refraction;
 
 
 import ut;
-import tardy.refraction: refractMixin;
+import tardy.refraction;
 import std.meta: AliasSeq;
 static import std.traits;  // because refract uses it
 
@@ -15,17 +15,69 @@ int simple(double d, string s);
 int safe_(ubyte b, float f, double d) @safe;
 double pure_(double) @safe pure;
 
+struct Struct {
+    import modules.types: Point;
+
+    int const_() const;
+    Point point() const;
+}
+
 alias functions = AliasSeq!(
+    Struct.const_,
+    Struct.point,
     simple,
     safe_,
     pure_,
 );
 
+pragma(msg, "");
 static foreach(F; functions) {
-    mixin(refractMixin!F(newId!F));
-    static assert(is(typeof(mixin(newId!F)) == typeof(F)));
+    pragma(msg, "F: ", __traits(identifier, F), "\t", methodRecipe!F);
+    mixin(methodRecipe!F, " ", methodId!F, ";");
+}
+pragma(msg, "");
+
+
+@("return")
+@safe pure unittest {
+    static foreach(F; functions) {
+        shouldMatchReturnType!F;
+    }
+}
+
+private void shouldMatchReturnType(alias F)() {
+    import std.traits: ReturnType;
+    import std.conv: text;
+    enum method = methodId!F;
+    static assert(is(ReturnType!F == ReturnType!(mixin(method))),
+                  text("Wrong return type for " ~ __traits(identifier, F),
+                       ": expected ", ReturnType!F.stringof, " but got ", ReturnType!(mixin(method)).stringof));
+}
+
+
+@("self")
+@safe pure unittest {
+    shouldMatchSelf!(simple, void*);
+}
+
+
+private void shouldMatchSelf(alias F, T)() {
+    import std.conv: text;
+    import std.traits: Parameters;
+
+    enum method = methodId!F;
+    alias params = Parameters!(mixin(method));
+
+    static assert(is(params[0] == T),
+                  text("Wrong self type for ", __traits(identifier, F),
+                       ": expected ", T.stringof, " but got ", params[0].stringof));
 }
 
 private string newId(alias F)() {
     return "as_" ~ __traits(identifier, F);
+}
+
+
+private string methodId(alias F)() {
+    return "asvtable_" ~ __traits(identifier, F);
 }

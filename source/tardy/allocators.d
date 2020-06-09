c5d1d359726b806b63cc94a514a2363edcaec514
Atila Neves <atila.neves@gmail.com>
2020-06-09 18:04:28 +0200

    allocate and deallocate unit tests

--- source/tardy/allocators.d
+++ source/tardy/allocators.d
@@ -1,5 +1,16 @@
 module tardy.allocators;
 
+
 struct InSitu(size_t N) {
-    ubyte[N] _;
+
+    private ubyte[N] _buffer;
+
+    void[] allocate(size_t n) {
+        return _buffer[0 .. n];
+    }
+
+    void deallocate(void[] buf) {
+        if(buf.ptr !is _buffer.ptr)
+            throw new Exception("Not my buffer");
+    }
 }
--- tests/ut/memory/allocators.d
+++ tests/ut/memory/allocators.d
@@ -4,8 +4,29 @@ module ut.memory.allocators;
 import ut;
 
 
-@("sizeof")
+@("insitu.sizeof")
 @safe pure unittest {
-    InSitu!16 insitu16;
-    static assert(insitu16.sizeof == 16);
+    static assert(InSitu!16.sizeof == 16);
+    static assert(InSitu!32.sizeof == 32);
+}
+
+
+@("insitu.allocate")
+@safe pure unittest {
+    import core.exception: RangeError;
+
+    InSitu!16 allocator;
+    auto buf = allocator.allocate(2);
+    buf.length.should == 2;
+    allocator.allocate(16);
+    allocator.allocate(17).shouldThrow!RangeError;
+}
+
+
+@("insitu.deallocate")
+@safe pure unittest {
+    InSitu!16 allocator;
+    void[] buf;
+    allocator.deallocate(buf).shouldThrow;
+    allocator.deallocate(allocator.allocate(5));
 }
